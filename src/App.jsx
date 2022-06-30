793c75002497ffb4141f8219df81f3cd94ba0538
Mateo <sanromanxp@gmail.com>
2022-06-29 23:29:18 -0500

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,27 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import logo from './logo.svg'
-import './App.css'
+import style from './index.module.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import useDimensions from "react-use-dimensions";
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+
+function Component() {
+
+    return (
+        <div className={style.component}>
+            <div className={style.body}>
+                <div className={style.component_title}>
+                    TRANSPARENCIA GENERAL
+                </div>
+                <div className={style.column}>
+                    <div className={style.container}> </div>
+                    <div className={style.container}> </div>
+                    <div className={style.container}> </div>
+                    <div className={style.container}> </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default App
+export default Component
