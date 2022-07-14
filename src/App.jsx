1af89d66e44524d15f978c8766387635c4570fcf
MATEO <sanromanxp@gmail.com>
2022-07-14 15:51:15 -0500

    v.01

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,60 @@
-import {useState} from 'react'
-import logo from './logo.svg'
+import {useEffect, useState} from 'react'
 import style from './index.module.css'
-
+import axios from 'axios'
 
 function Component() {
+    const [data, setData] = useState([])
+
+    const getData = async () => {
+        try {
+            let response = await axios.get(import.meta.env.VITE_API_URL + 'tipos?populate=componentes');
+            setData(response.data.data)
+        } catch (e) {
+            console.log(e)
+            setData([])
+        }
+
+    }
+
+    useEffect(() => {
+        getData()
+    }, []);
+
+    console.log('DATA')
+    console.log(data)
+
+    let children = null
+    if (data.length) {
+        children = data.map((item) => {
+            console.log('item')
+            console.log(item)
+            let gchildren = item.attributes.componentes.data.map((child) => {
+                console.log('gchild')
+                console.log(child)
+                return (
+                    <div className={style.container}>
+                        {child.attributes.titulo}
+                    </div>
+                )
+            })
+
+            return (
+                <>
+                    <div className={style.component_title}>{item.attributes.titulo}</div>
+                    <div className={style.column}>
+                        {gchildren}
+                    </div>
+                </>
+            )
+        })
+    }
+
 
     return (
         <div className={style.component}>
+            <img className={style.banner} src="./assets/images/banner.jpg"/>
             <div className={style.body}>
-                <div className={style.component_title}>
-                    TRANSPARENCIA GENERAL
-                </div>
-                <div className={style.column}>
-                    <div className={style.container}> </div>
-                    <div className={style.container}> </div>
-                    <div className={style.container}> </div>
-                    <div className={style.container}> </div>
-                </div>
+                {children}
             </div>
         </div>
     )
